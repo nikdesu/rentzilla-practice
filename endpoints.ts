75a91b0b8d89812020b4605da03f99c116631052
nikdesu <ostspchyk1409@gmail.comf>
2024-03-04 09:43:31 +0200

    deleted unused endpoints

--- endpoints.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  privacy_policy: "/privacy-policy/",
-  cookie_policy: "/cookie-policy/",
-  terms_conditions: "/terms-conditions/",
-  products: "/products/",
-  tenders: "/tenders-map/",
-  chosen_product: "/unit/",
-};
