156ec77fb0356ffa73a1ad3e850de6e605448474
nikdesu <nikdesu@users.noreply.github.com>
2024-03-04 09:04:12 +0000

    deploy: 05f43c82221dc08971463fb02b308b4882b62e95

--- endpoints/storageEq.endpoints.ts
+++
@@ -1,4 +0,0 @@
-export const storageEqEndpoints = {
-  loadersEq: "ekskavatori-dlya-znesennia",
-  storageEquipment: "obladnannia-dlya-navantazhuvachiv",
-};
--- endpoints/utilityEq.endpoints.ts
+++
@@ -1,8 +0,0 @@
-export const utilityEqEndpoints = {
-  emergency: "avariini-mashini",
-  roadCleaning: "dorozhno-pribiralna-tekhnika",
-  cleaning: "kliningove-obladnannia",
-  containers: "komunalni-konteineri",
-  transport: "komunalni-mashini",
-  equipment: "tekhnika-dlya-skladuvannia",
-};
