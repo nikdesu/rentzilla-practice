2ebb029160e5157242b71619293a3e894c1feb67
nikdesu <ostspchyk1409@gmail.comf>
2024-03-04 09:42:40 +0200

    added new fixtures

--- fixtures/base.fixtures.ts
+++ fixtures/base.fixtures.ts
@@ -3,8 +3,16 @@ import { test as base } from "@playwright/test";
 import HomePage from "@pages/home.page";
 import ProductsPage from "@pages/products.page";
 import ChosenProductPage from "@pages/chosenProduct.page";
+import BackcallApi from "@api/backcall.api";
+import HelperApi from "@api/helper.api";
 
-export const test = base.extend<{ homePage: HomePage; productsPage: ProductsPage; chosenProductPage: ChosenProductPage }>({
+export const test = base.extend<{
+  homePage: HomePage;
+  productsPage: ProductsPage;
+  chosenProductPage: ChosenProductPage;
+  backcallApi: BackcallApi;
+  helperApi: HelperApi;
+}>({
   homePage: async ({ page }, use, TestInfo) => {
     await use(new HomePage(page, TestInfo));
   },
@@ -14,6 +22,12 @@ export const test = base.extend<{ homePage: HomePage; productsPage: ProductsPage
   chosenProductPage: async ({ page }, use, TestInfo) => {
     await use(new ChosenProductPage(page, TestInfo));
   },
+  backcallApi: async ({ request }, use) => {
+    await use(new BackcallApi(request));
+  },
+  helperApi: async ({ backcallApi }, use) => {
+    await use(new HelperApi(backcallApi));
+  },
 });
 
-export { expect, type Page, type Locator, type TestInfo } from "@playwright/test";
+export { expect, type Page, type Locator, type TestInfo, type APIRequestContext } from "@playwright/test";
